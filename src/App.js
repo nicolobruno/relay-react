00820d5396301dd0722d0009d34956dd3dceb3ff
nicolas <nicolas@cookunity.com>
2020-02-07 09:41:46 -0300

    Kickoff

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+import fetchGraphQL from './fetchGraphQL';
+
+const { useState, useEffect } = React;
+
+function App() {
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        # feel free to change owner/name here
+        repository(owner: "facebook" name: "relay") {
+          name
+        }
+      }
+    `).then(response => {
+      if (!isMounted) {
+        return;
+      }
+      const data = response.data;
+      setName(data.repository.name);
+    }).catch(error => {
+      console.error(error);
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [fetchGraphQL]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {name != null ? `Repository: ${name}` : "Loading"}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
